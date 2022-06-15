4a8c9f8133851fd89e7bd34ace79b59a56def43a
Leonardo Lima <eng.leolsouza@hotmail.com>
2022-06-15 10:11:00 -0300

    starting_new_project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import Todo from "./components/Todo";
+
 
 function App() {
-  return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo text='Learn React'/>  
-      <Todo text='Master React'/>  
-      <Todo text='Explore the full React course'/>  
-      
+  return <div>
+    
     </div>
-  );
+  ;
 }
 
 export default App;
