bf89380214137609d3dee77544388f40ef42d357
Leonardo Lima <eng.leolsouza@hotmail.com>
2022-06-15 09:26:32 -0300

    adding_click_to_exit_for_modal

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -22,15 +22,16 @@ function Todo(props) {
       <div className="actions">
         <button className="btn" onClick={deleteHandler}>Delete</button>
       </div>
-      {modalIsOpen && <Modal />}
+      {modalIsOpen && <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler}/>}
       {modalIsOpen && <Backdrop onClick={closeModalHandler}/>}
     </div>
   );
 }
 
-export default Todo;
+export default Todo; 
 /*Para deixar mais dinamico é inserido um objeto, nesse caso props, que pode
 ser qualquer coisa, mas é melhor usar props, no qual chama dentro de {} q seria
 para chamar o js, com .text para chamar o valor dado la no App.js */
 /*{modalIsOpen ? <Modal /> : null} se isso for verdadeiro renderiza modal se não, não renderiza nada*/ 
-/*{modalIsOpen && <Modal />} mesma coisa porém se a primeira e segunda opção são verdadeiras, então renderiza a segunda função*/
+/*{modalIsOpen && <Modal />} mesma coisa porém se a primeira e segunda opção são verdadeiras, então renderiza a segunda função, isso é o que faz abrir a janela ao clicar no delete, 
+modalIsOpen define que o será aberto no useState*/
