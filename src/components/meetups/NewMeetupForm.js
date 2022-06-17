de217b8f86f178afd75f43b0c663932ca156156e
Leonardo Lima <eng.leolsouza@hotmail.com>
2022-06-17 09:42:49 -0300

    creating_store_the_data

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,25 +1,50 @@
+import { useRef } from "react";
+
 import Card from "../ui/Card";
 import classes from "./NewMeetupForm.module.css";
 
 function NewMeetupForm() {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();  
+
+  function submitHandler(event){
+    event.preventDefault();
+
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const meetupData = {
+        title : enteredTitle,
+        image : enteredImage,
+        address: enteredAddress,
+        description : enteredDescription,
+    };
+
+    console.log(meetupData);
+  }  
+
   return (
     <Card>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="title">Meetup Tittle</label>
-          <input type="text" required id="title" />
+          <input type="text" required id="title" ref={titleInputRef}/>
         </div>
         <div className={classes.control}>
           <label htmlFor="image">Meetup Image</label>
-          <input type="url" required id="image" />
+          <input type="url" required id="image" ref={imageInputRef}/>
         </div>
         <div className={classes.control}>
           <label htmlFor="address">Address</label>
-          <input type="text" required id="address" />
+          <input type="text" required id="address" ref={addressInputRef}/>
         </div>
         <div className={classes.control}>
           <label htmlFor="description">Description</label>
-          <textarea required id="description" rows='5'/>
+          <textarea required id="description" rows='5' ref={descriptionInputRef}/>
         </div>
         <div className={classes.actions}>
             <button>Add Meetup</button>
@@ -29,3 +54,9 @@ function NewMeetupForm() {
   );
 }
 export default NewMeetupForm;
+
+/*preventDefault evita o comportamento padrão do navegador que seria enviar 
+a request para o servidor;
+useRef é utilizado para resgastarmos o que foi inserido no campo pra pode fazer oq quiser;
+const enteredTitle = titleInputRef.current.value sendo current para pegar o valor atual, e value mantém desses valores atuais 
+*/
